c5f8d480b6ea8da5c2293218b83dbc19333c2095
Siri Devarapalli <66244797+sirideva4@users.noreply.github.com>
2022-09-25 13:05:13 -0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,29 @@
+<iframe width="425" height="350" 
+        frameborder="0" 
+        scrolling="no" 
+        marginheight="0" 
+        marginwidth="0" 
+        src="https://maps.google.com/maps?f=q&
+        source=s_q&
+        hl=en&
+        geocode=&
+        q=333+E+34th+St,+New+York,+NY&
+        aq=1&
+        oq=333&
+        sll=37.269174,-119.306607&
+        sspn=16.742323,33.815918&
+        ie=UTF8&
+        hq=&
+        hnear=333+E+34th+St,+New+York,+10016&
+        t=m&
+        z=14&
+        ll=40.744403,-73.974467&
+        output=embed">
+</iframe>
+
+<code<div class="directions"><form action="http://maps.google.com/maps" method="get" target="_blank">
+   <label for="saddr" class="dir-label">Enter Your Location for Directions</label>
+   <input type="text" name="saddr" class="dir-input" size="16" />
+   <input type="hidden" name="daddr" value="A List Apart, 333 E 34th St, New York, NY" />
+   <input type="submit" value="Get directions" class="dir-button button" />
+</form></div>
